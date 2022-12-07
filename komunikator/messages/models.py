a3245f689d99e1f65bf0c3ce41b03bb4c53a2e67
Paweł Janicki <pawel.jan526@gmail.com>
2022-12-07 14:13:43 +0100

    prototype models

--- komunikator/messages/models.py
+++ komunikator/messages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from komunikator.accounts.models import User
 
 # Create your models here.
+
+class Messages(models.Model):
+
+    from_id = models.ForeignKey(User, default=None, on_delete=models.SET_DEFAULT, null=False, blank=False)
+    to_id = models.ForeignKey(User, default=None, on_delete=models.SET_DEFAULT, null=False, blank=False)
+    message = models.CharField(max_length=1000)
+    date_time = models.DateTimeField()
